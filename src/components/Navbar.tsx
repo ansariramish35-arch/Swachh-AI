2c664cee769c6f0183b691923436c7926607deae
Mohammad Ramish Ansari <ansariramish35@gmail.com>
2026-09-25 22:39:47 +0530

    Throttle navbar scroll updates with RAF

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,10 +18,22 @@ export default function Navbar() {
   const progress = useSpring(scrollYProgress, { stiffness: 90, damping: 20 });
 
   useEffect(() => {
-    const onScroll = () => setScrolled(window.scrollY > 24);
+    let rafId = 0;
+
+    const onScroll = () => {
+      cancelAnimationFrame(rafId);
+      rafId = requestAnimationFrame(() => {
+        setScrolled(window.scrollY > 24);
+      });
+    };
+
     onScroll();
     window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+      cancelAnimationFrame(rafId);
+    };
   }, []);
 
   return (
